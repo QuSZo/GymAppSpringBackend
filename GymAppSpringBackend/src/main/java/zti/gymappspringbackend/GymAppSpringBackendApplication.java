d59b022371124955dd05977866dd65415206a0ea
Mikołaj Kuszowski <mikolajkuszowski@gmail.com>
2025-05-08 21:07:18 +0200

    Created project

---
+++ GymAppSpringBackend/src/main/java/zti/gymappspringbackend/GymAppSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package zti.gymappspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymAppSpringBackendApplication.class, args);
+    }
+
+}
---
+++ GymAppSpringBackend/src/test/java/zti/gymappspringbackend/GymAppSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zti.gymappspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
